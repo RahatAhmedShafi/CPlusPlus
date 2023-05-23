c5f4991e1dcd05125c2fc90d383d65abd859e407
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-05-23 12:48:22 +0600

    Operator
    Some Operator

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    int a;
+//    int b;
+//    float f1=34e3;
+//    double d1=23E4;
+//    char x=65,y=66,z=67;
+//    cout<<"Enter a number: ";
+//    cin>>a;
+//    cout<<"Enter b number: ";
+//    cin>>b;
+//    int sum=a+b;
+//    bool bol1=true;
+//    string st="Ban";
+//
+//    cout<<f1<<"\n";
+//    cout<<d1<<"\n";
+//    cout << sum<<"\n";
+//    cout<<st<<"\n";
+//    cout<<bol1<<"\n";
+//    cout<<x<<"\n";
+//    cout<<y<<"\n";
+//    cout<<z<<"\n";
+
+                    /*        OPERATOR               */
+
+
+//  int x =11;
+//  x<<=2;  // Left shift operator x<<=2 that means 2^2=4 and result is 4*11=44
+//  cout<<x;
+//   int y=44;
+//   y>>=2;// Right shift operator y>>=2 that means 2^2=4 and result is 44/4=11
+//   cout<<y;
+//  int a=11;
+//  int b=2;
+//  int z =a&b;// And operator means: if a=11 and b=2 their binary number is a=1011,b=0010 then result is 2 ;logic gate & formula use
+//  cout <<z ;
+//    int r=2;
+//    r^=2; // Logic gate XOR formula use
+//    cout<<r;
+//    int p=0;
+//    p|=2;
+//    cout<<p;
+int
+    return 0;
+}
