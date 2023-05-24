eafb68c9cb010f3ec57722c12bc47885038b1220
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-05-24 21:35:06 +0600

    While loop

--- main.cpp
+++ main.cpp
@@ -157,6 +157,17 @@ int main()
 //     cout<<"\n";
 //    }
 
+       int i = 0;
+        while (i < 10) {
+          if (i == 4) {
+            i++;
+            continue;
+
+          }
+          cout << i << "\n";
+          i++;
+        }
+
 
 
 
