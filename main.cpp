ceec601fd62de04a8734c4303c529f049d782b6c
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-05-24 19:22:55 +0600

    Condition Statement

--- main.cpp
+++ main.cpp
@@ -101,34 +101,34 @@ int main()
 
              /*     C++ Switch Statements     */
 
-      int Day=5;
-      switch(Day){
-        case 1:
-        cout<<"Saturday";
-        break;
-        case 2:
-        cout<<"Sunday";
-        break;
-        case 3:
-        cout<<"Monday";
-        break;
-        case 4:
-        cout<<"Tuesday";
-        break;
-        case 5:
-        cout<<"Wednesday";
-        break;
-        case 6:
-        cout<<"Thursday";
-        break;
-        case 7:
-        cout<<"Friday";
-        break;
-
-
-
-
-      }
+//      int Day=5;
+//      switch(Day){
+//        case 1:
+//        cout<<"Saturday";
+//        break;
+//        case 2:
+//        cout<<"Sunday";
+//        break;
+//        case 3:
+//        cout<<"Monday";
+//        break;
+//        case 4:
+//        cout<<"Tuesday";
+//        break;
+//        case 5:
+//        cout<<"Wednesday";
+//        break;
+//        case 6:
+//        cout<<"Thursday";
+//        break;
+//        case 7:
+//        cout<<"Friday";
+//        break;
+//
+//
+//
+//
+//      }
 
 
 
